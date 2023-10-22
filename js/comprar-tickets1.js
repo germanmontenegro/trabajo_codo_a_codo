8ba9d19b27923177e70ef448b79a6ebdb0969bde
Germán Montenegro <46161545+germanmontenegro@users.noreply.github.com>
2023-10-22 01:51:00 -0300

    Add files via upload
    Signed-off-by: Germán Montenegro <46161545+germanmontenegro@users.noreply.github.com>

---
+++ js/comprar-tickets1.js
@@ -0,0 +1,22 @@
+// Defino valor de ticket
+const valorTicket = 200;
+    function CargarDatos()
+    {
+        document.getElementById("demo")=200;
+    }
+function CalcularPrecio(a,b,c)
+{   
+    return(a*b-c);
+}
+function Opcion(valortemporal) {
+    let valortemporal;
+    let resultado;
+    if (valortemporal=="estudiante")
+    resultado=Document.getSelect($(Calcularprecio)-Calcular*0.80);
+     return(resultado);
+    else 
+       if (valortemporal=="trainner")
+       resultado=Document.getSelect($(CalcularPrecio-CalcularPrecio*0.50);
+       else resultado=Document.getSelect($(CalcularPrecio)-CalcularPrecio*0.15)
+    }
+
